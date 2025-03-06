170b2464be0ede2d40ddb6e190e63aae041c2cde
chenzm <chenzm@cloudglab.com>
2025-03-06 19:46:51 +0800

    新增信息推送至Categrafurl

--- main.go
+++ main.go
@@ -11,9 +11,9 @@ import (
 
 func main() {
 	// 命令行参数
-	baseURL := flag.String("url", "http://192.168.45.10", "API基础URL")
+	baseURL := flag.String("url", "http://127.0.0.1", "API基础URL")
 	username := flag.String("username", "admin", "API用户名")
-	password := flag.String("password", "Yskj@2022@", "API密码")
+	password := flag.String("password", "123456", "API密码")
 	notifyCategraf := flag.Bool("notifyCategraf", false, "是否通知Categraf")
 	CategrafUrl := flag.String("CategrafUrl", "http://192.168.45.10", "N9E基础URL")
 	flag.Parse()
